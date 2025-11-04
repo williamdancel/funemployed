54906dbf80797d1db338e2352341ab304c614f07
williamdancel <william.a.dancel@gmail.com>
2025-11-05 02:51:17 +0800

    feat: * upgrade versions of vue, tailwind and postcss. (package.json) * installed vue-router (package.json) * added plugin-vue in vite.config.js * added app.blade.php

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
+// resources/js/app.js
 import './bootstrap';
+import { createApp } from 'vue';
+import router from './router/index.js';
+import App from './components/App.vue';
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any?}', function () {
+    return view('app');
+})->where('any', '.*');
